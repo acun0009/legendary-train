1289344e76889aa6e5e24d620c904e54fd9c8cc0
Atlas <acun0009@algonquinlive.com>
2024-10-11 16:53:07 -0400

    end of class

--- js/data.js
+++ js/data.js
@@ -1,1 +1,12 @@
-export let enemyList = [];
+export let enemyList = [
+    {
+        uuid: 'fddea26e-d0b2-456b-bcd1-71e900f32c2d',
+        name: "Mother Theresa",
+        reason: "Did not come to my birthday."
+    },
+    {
+        uuid: '5be481ac-45ba-45d8-8535-a9439b7c35b9',
+        name: "Ghengis Khan",
+        reason: "Called my brother mean things."
+    }
+];
